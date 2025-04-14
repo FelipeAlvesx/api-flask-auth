6e389f05775163d2aa85b2075f456027421c8988
felipe oliveira <felipeoliveira@felipes-MacBook-Air.local>
2025-04-14 09:44:43 -0300

    update login view

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from secret import SECRET_KEY
 from models.users import User
 from database import db
 from login_meneger_file import login_manager
+from flask_login import login_user
 
 app = Flask(__name__)
 
@@ -11,17 +12,29 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db.init_app(app)
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@app.route("/hello-wolrd", methods=["GET"])
+def hello_world():
+    return "HELLO WORLD"
 
 
 @app.route("/login", methods=["POST"])
 def login():
-    data = request.json()
+    data = request.json
     username = data.get('username')
     email = data.get('email')
-    password = data.get('password')
+    password = data.get('password') 
     
     if email and password:
-        ...
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            login_user(user) # authentication
+            return jsonify({"message": "login successfuly"})
+    
+    return jsonify({"message": "invalid credentials"}), 400
+
 
 if __name__ == "__main__":
     app.run(
